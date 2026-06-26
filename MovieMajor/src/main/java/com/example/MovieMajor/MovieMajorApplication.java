c71e0d8b47ff354f29e6499260ada3c9c687b5f6
hiitarun1 <ktarunkushwaha@gmail.com>
2026-06-26 23:40:36 +0530

    Initial commit: project skeleton from Spring Initializr

---
+++ MovieMajor/src/main/java/com/example/MovieMajor/MovieMajorApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieMajor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMajorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMajorApplication.class, args);
+	}
+
+}
---
+++ MovieMajor/src/test/java/com/example/MovieMajor/MovieMajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieMajor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
